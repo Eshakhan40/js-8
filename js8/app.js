0acd5814e5888cacd98962fc6a5be3c416c59b4c
Eshakhan40 <159137900+Eshakhan40@users.noreply.github.com>
2024-08-05 12:01:10 -0700

    Add files via upload

---
+++ js8/app.js
@@ -0,0 +1,69 @@
+// function date(){
+//     document.write(new Date())
+// }
+// date()
+// function greeting(){
+//     var a=prompt("Enter our first name")
+//     var  b=prompt("Enter our last name")
+//     document.write("Hello"+ a+b)
+// }
+// greeting("hello")
+// var user1=+prompt("Enter num")
+// var user2=+prompt("Again enter any num")
+// document.write(user1+user2)
+// function operator(){
+//     var num1=+prompt("Enter first num")
+//     var num2=+prompt("ENter Second num")
+//      var num3=+prompt("Enter operator:+ ,-,*,/")
+//     if(num3==="+"){
+//         alert("Sum of:"+ num1+ "and"+num2 +"is"+(num1+num2) )
+//     }
+//     else if( num3==="-"){
+//         alert("Sub of:"+ num1+ "and"+num2 +"is"+(num1-num2))
+//     }
+//     else if( num3==="*"){
+//         alert(" X of:"+ num1+ "and"+num2 +"is"+(num1*num2))
+//     }
+    
+//     else if(num3==="/"){
+//         alert(" div of:"+ num1+ "and"+num2 +"is"+(num1/num2))
+//     }
+// }
+// operator ()
+// function b(){
+//     var u=+prompt("which num you need to square?")
+//     alert( "square of desire num is:"+u*u)
+
+// }
+// b()
+//  function factorial (){
+//      var a = parseInt(prompt("enter a number for factorial "))
+//      if (a < 0){
+//          alert("factorial is not defined for neg values")
+//      } else{
+//          var result=1
+//      for(var i =1 ; i<=a; i++){
+//          result*=i
+//      }
+//      alert( "the factorial of " + a+ " is " + result) 
+//      }
+// }
+//  factorial()
+// function num(){
+//     var a=+prompt("Enter starting num")
+//     var b=+prompt("Enter end num")
+//     for( i=a; i<=b; i++){
+//         document.write(i+"<br>")
+//     } 
+// }
+// num()
+// function hypotenuse(){
+//     function square(x){
+//         return x*x
+//     }
+//     var h=+prompt("Enter the value of base")
+//     var p=+prompt("Enter the value of perpendicular ")
+//     var c=Math.sqrt( square(h)+square(p))
+//     alert("The hypotenuse is "+  c )
+// }
+// hypotenuse()
